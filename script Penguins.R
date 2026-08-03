f6e6a7dff3ae25a71434542a3aabeba4bdf24e83
Sam-ai06 <sammyvilla3006@gmail.com>
2026-08-03 18:58:03 -0500

    script R
    inicio del archivo R

---
+++ script Penguins.R
@@ -0,0 +1,9 @@
+#librerías para cargar la data 
+library(readxl)
+
+#librerías para exploración y limpieza
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+
+penguins <- read_excel("6_penguins_lter.xlsx")
